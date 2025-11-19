f2dcf1f719bcb0e468ffe45d58b3e50f5b71c6d7
Qualyyy <Jarne.QUALY@student.howest.be>
2025-11-19 16:08:04 +0100

    generateFile: get targetPath

--- src/commands/generateFile.ts
+++ src/commands/generateFile.ts
@@ -1,10 +1,18 @@
 import * as vscode from 'vscode';
 import { getConfig } from '../utils/configUtils';
 import { validateConfigTemplatesDirectory } from '../utils/validation';
+import { getTargetPath } from '../utils/pathUtils';
 export async function generateFileCommand(Uri?: vscode.Uri) {
     // Get the user's set templatesDirectory
     const templatesDirectory = (await getConfig()).templatesDirectory;
     if (!(await validateConfigTemplatesDirectory(templatesDirectory))) {
         return;
     }
+
+    // Get the target path
+    const targetPath = (await getTargetPath(Uri)).targetPath;
+    if (!targetPath) {
+        return;
+    }
+    console.log(targetPath);
 }
