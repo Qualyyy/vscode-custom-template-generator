f5d81f1e8821460471f6e16774e549478ae8a124
Qualyyy <Jarne.QUALY@student.howest.be>
2025-11-19 19:43:31 +0100

    generateFile: create the file from the template if it doesn't exist yet

--- src/commands/generateFile.ts
+++ src/commands/generateFile.ts
@@ -1,8 +1,10 @@
 import * as vscode from 'vscode';
+import * as fs from 'fs';
 import { getConfig } from '../utils/configUtils';
 import { validateConfigTemplatesDirectory } from '../utils/validation';
 import { getTargetPath } from '../utils/pathUtils';
 import { promptTemplateSelect } from '../utils/promptUtils';
+import * as path from 'path';
 
 export async function generateFileCommand(Uri?: vscode.Uri) {
     // Get the user's set templatesDirectory
@@ -23,7 +25,19 @@ export async function generateFileCommand(Uri?: vscode.Uri) {
     if (!templatePath) {
         return;
     }
-    console.log(templatePath);
+    const templateContent = fs.readFileSync(templatePath, 'utf8');
+    const templateName = path.basename(templatePath);
+
+    const filePath = path.join(targetPath, templateName);
+
+    console.log(templatePath, filePath);
+
+    if (fs.existsSync(filePath)) {
+        vscode.window.showErrorMessage('File already exists', { modal: true });
+        return;
+    }
+
+    fs.writeFileSync(filePath, templateContent);
 
     if (createNewFolder) {
         vscode.commands.executeCommand('vscode.openFolder', vscode.Uri.file(targetPath), false);
