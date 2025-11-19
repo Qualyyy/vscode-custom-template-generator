b9cdb0d386c66b0b0735bb530e6f8e268dcad0be
Qualyyy <Jarne.QUALY@student.howest.be>
2025-11-19 15:28:42 +0100

    generateFile: get templatesDirectory

--- src/commands/generateFile.ts
+++ src/commands/generateFile.ts
@@ -1,4 +1,7 @@
 import * as vscode from 'vscode';
+import { getConfig } from '../utils/configUtils';
 export async function generateFileCommand(Uri?: vscode.Uri) {
     console.log('Hello world!');
+
+    const templatesDirectory = (await getConfig()).templatesDirectory;
 }
