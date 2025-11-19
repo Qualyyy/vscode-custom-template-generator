2812db6f071b67e2cade08e724a8af5ca7b18648
Qualyyy <Jarne.QUALY@student.howest.be>
2025-11-19 15:40:32 +0100

    generateFile: remove 'Hello world' log

--- src/commands/generateFile.ts
+++ src/commands/generateFile.ts
@@ -2,8 +2,6 @@ import * as vscode from 'vscode';
 import { getConfig } from '../utils/configUtils';
 import { validateConfigTemplatesDirectory } from '../utils/validation';
 export async function generateFileCommand(Uri?: vscode.Uri) {
-    console.log('Hello world!');
-
     // Get the user's set templatesDirectory
     const templatesDirectory = (await getConfig()).templatesDirectory;
     if (!(await validateConfigTemplatesDirectory(templatesDirectory))) {
