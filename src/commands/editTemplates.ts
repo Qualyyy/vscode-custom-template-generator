1b4ad1556ce21c1f71352bc82b585eb1e5681aa4
Qualyyy <Jarne.QUALY@student.howest.be>
2025-11-20 13:53:09 +0100

    editTemplates: open the templatesDirectory in new window when command is ran

--- src/commands/editTemplates.ts
+++ src/commands/editTemplates.ts
@@ -1,3 +1,14 @@
+import * as vscode from 'vscode';
+import { getConfig } from '../utils/configUtils';
+import { validateConfigTemplatesDirectory } from '../utils/validation';
+
 export async function editTemplatesCommand() {
-    console.log("Hello World!");
+    // Get the user's set templatesDirectory
+    const templatesDirectory = (await getConfig()).templatesDirectory;
+    if (!(await validateConfigTemplatesDirectory(templatesDirectory))) {
+        return;
+    }
+
+    // Open templatesDirectory in new window
+    vscode.commands.executeCommand('vscode.openFolder', vscode.Uri.file(templatesDirectory), true);
 }
